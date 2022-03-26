911fe1d4b4950a57d6eefc58815ff726ad54cf5d
Breno-MT <hentgenp@gmail.com>
2022-03-25 22:30:52 -0300

    Modificado Certas Linhas
    Adicionei 2 imagens pro html (Deletar e Editar), e modifiquei linhas, corrigindo bugs e estou adicionando uma função editar.

--- ExerciciosM1S04/script.js
+++ ExerciciosM1S04/script.js
@@ -1,6 +1,6 @@
 class Produto {
     constructor(){
-        this.id = 0;
+        this.id = 1;
         this.arrayProdutos = [];
     }
 
@@ -8,14 +8,106 @@ class Produto {
         let produto = this.lerDados();
 
         if (this.validaCampos(produto)) {
-            alert('salvar');
+            this.adicionar(produto);
         }
 
-        console.log(produto);
+        this.listaTabela();
+        this.cancelar();
+    }
+
+    listaTabela() {
+        let tbody = document.getElementById("tbody");
+
+        tbody.innerText = '';
+
+        
+        for (let i = 0; i < this.arrayProdutos.length; i++) {    
+
+            let tr = tbody.insertRow();
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            
+            
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].valorProduto;
+            
+            
+            td_id.classList.add('center');
+            td_acoes.classList.add('center');
+            
+            
+            
+            
+            let imgEdit = document.createElement('img');
+            imgEdit.src="./iconsHTML/editIcon.png";
+            //imgEdit.setAttribute("onclick");
+            td_acoes.appendChild(imgEdit);
+            
+            if (imgEdit && imgEdit.style){
+                imgEdit.style.maxHeight = '25px';
+                imgEdit.style.maxWidth= '25px';
+
+            }
+            
+            
+            let imgDelete = document.createElement('img');
+            imgDelete.src="./iconsHTML/delete.jpg";
+            imgDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
+            td_acoes.appendChild(imgDelete);
+            
+            if (imgDelete && imgDelete.style){
+                imgDelete.style.maxHeight = '25px';
+                imgDelete.style.maxWidth= '25px';
+
+            }
+    
+        }
+    }
+
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
     }
 
     cancelar() {
+        document.getElementById("produto").value='';
+        document.getElementById("valor").value='';
     }
+
+    deletar(id) {
+        let tbody = document.getElementById("tbody");
+
+        if(confirm("Deseja excluir o id: " + id)){
+
+            for (let i = 0; i < this.arrayProdutos.length; i++) {
+    
+                
+                if(this.arrayProdutos[i].id == id) {
+                    this.arrayProdutos.splice(i, 1)
+                    tbody.deleteRow(i);
+                }
+            }
+        }
+
+
+    }
+
+    editar(i) {
+
+        // Não está completo.
+
+        this.arrayProdutos[i] == i;
+
+        this.arrayProdutos.splice(i, 1, nomeProduto, valorProduto)
+
+        
+
+    }
+
 
     lerDados() {
         let produto = {}
@@ -24,6 +116,7 @@ class Produto {
         produto.nomeProduto = document.getElementById("produto").value;
         produto.valorProduto = document.getElementById("valor").value;
 
+        console.log(produto)
         return produto;
     }
 

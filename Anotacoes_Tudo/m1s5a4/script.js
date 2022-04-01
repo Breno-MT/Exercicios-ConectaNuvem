2738b858fff4234fa065f12907d8d99ce3c2e76c
Breno-MT <hentgenp@gmail.com>
2022-03-31 21:49:03 -0300

    update m1s5a4

--- Anotacoes_Tudo/m1s5a4/script.js
+++ Anotacoes_Tudo/m1s5a4/script.js
@@ -4,6 +4,8 @@ const listaCEPs = {
     }
 }
 
+
+// EXEMPLO DE PROMISE
 function buscaCEPpromise(cep){
 
     //Promise(resolve, reject) <- já é pré-definido, só passar os nomes que quer usar no lugar,
@@ -31,5 +33,142 @@ buscaCEPpromise('88034001')
      console.log(erro);
  })
  .finally(() => {
-     console.log("Fim :D")
+     console.log("Sempre exec no fim de tudo")
  });
+
+
+// EXEMPLO DE FETCH
+
+const p = document.getElementById('display');
+const campoCEP = document.getElementById('campo-cep');
+const btnCEP = document.getElementById('btn-cep');
+
+
+
+// function buscaCEPfetch() {
+
+//     const cep = campoCEP.value;
+
+//     if(!cep) {
+//         display.innerHTML = 'Informe um CEP!';
+//         return;
+//     }
+
+//     fetch(`https://viacep.com.br/ws/${cep}/json`)
+//       .then(resposta => {
+//         resposta.json()
+//           .then(conteudo => {
+//             console.log(conteudo);
+//             p.innerHTML = conteudo.logradouro;
+//           })
+//       })
+//       .catch(erro => {
+//         console.log(erro)
+//       });
+    
+//     fetch('/Anotacoes_Tudo/m1s5a4/texto.txt')
+//         .then(resposta => {
+//             resposta.text().then(conteudo => {
+//                 console.log(conteudo);
+//             })
+//         })
+//         .catch(erro => {
+//             console.log(erro)
+//         })
+
+
+// }
+
+//btnCEP.addEventListener('click', buscaCEPfetch);
+
+// ASYNC WAIT
+
+// promise com atraso
+// function delay(ms) {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve('blah'), ms);
+//     })
+// }
+
+//   // uma arrow function assíncrona
+// async function buscaCEP(){
+
+//     //comando de espera
+//     const resposta = await delay(1000);
+
+//     //executa após conclusão de delay
+//     console.log('Terminei de esperar! ' + resposta)
+// }
+
+// roda o exemplo
+//buscaCEP();
+// imprime primeiro no console
+//console.log('Executei o console log');
+
+
+// async wait com fetch
+
+// async function buscaCEPawait() {
+
+//     const cep = campoCEP.value;
+
+//     if(!cep) {
+//         display.innerHTML = 'Informe um CEP!';
+//         return;
+//     }
+
+//     const url = `https://viacep.com.br/ws/${cep}/json`
+
+//     const resposta = await fetch(url);
+    
+//     const conteudo = await resposta.json();
+
+//     console.log(conteudo)
+//     display.innerHTML = conteudo.localidade;
+ 
+//       //.catch(erro => {
+//         //console.log(erro)
+//       //});
+    
+// }
+// btnCEP.addEventListener('click', buscaCEPawait);
+
+
+// EXEMPLO COM TRY CATCH
+
+function validaCEP(cep) {
+    if (cep.length !== 8) {
+        return false;
+    } 
+    return true;
+}
+
+
+async function buscaCEPtry() {
+
+    try {
+        const cep = campoCEP.value;
+    
+        if(validaCEP(cep)) {
+            throw new Error('CEP não informado!');
+        }
+    
+        const url = `https://viacep.com.br/ws/${cep}/json`
+    
+        const resposta = await fetch(url);
+        
+        const conteudo = await resposta.json();
+    
+        console.log(conteudo)
+        display.innerHTML = conteudo.localidade;
+
+    } catch (erro) {
+
+        console.log(erro);
+        console.log('!!! DENTRO DO CATCH !!!');
+        console.log(erro)
+    }
+    
+    
+}
+btnCEP.addEventListener('click', buscaCEPtry);

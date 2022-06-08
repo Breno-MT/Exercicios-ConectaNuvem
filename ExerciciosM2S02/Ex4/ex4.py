649267ad4ab61cb665f15582fcc59ccae0d567bb
Breno <hentgenp@gmail.com>
2022-06-07 22:29:14 -0300

    Create ExM2S02 - Ex4
    Criado a pasta contendo os exercicios do Modulo 2 da Semana 2, e já foi adicionado o Ex4 nele.

---
+++ ExerciciosM2S02/Ex4/ex4.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite um número [1]:"))
+num2 = int(input("Digite um número [2]:"))
+
+
+if num1 > num2:
+    print(f"O {num1} é maior que {num2}")
+
+if num1 < num2:
+    print(f"O {num1} é menor que {num2}")
+
+if num1 == num2:
+    print("Os números são iguais!")

1f59a6e7b0aba5a5b4fc08fa8fcdd04be93fe5be
Breno <hentgenp@gmail.com>
2022-06-14 03:29:16 -0300

    Update main.py
    Mudado os nomes das variaveis:
    dicionario_nome > nome_dict
    dicionario_sobrenome > sobrenome_dict
    dicionario_idade > idade_dict
    dicionario_user > dict_user
    dicionario_user_json > dict_user_json

--- ExerciciosM2S03/Ex08/main.py
+++ ExerciciosM2S03/Ex08/main.py
@@ -8,14 +8,14 @@ dicionario_json = json.dumps(dicionario_normal)
 
 print(dicionario_json)
 
-dicionario_nome = str(input("Digite o nome: "))
-dicionario_sobrenome = str(input("Digite o sobrenome: "))
-dicionario_idade = int(input("Digite a idade: "))
+nome_dict = str(input("Digite o nome: "))
+sobrenome_dict = str(input("Digite o sobrenome: "))
+idade_dict = int(input("Digite a idade: "))
 
-dicionario_user = {   "primeiro_nome": dicionario_nome, 
-                                        "segundo_nome": dicionario_sobrenome, 
-                                        "idade": dicionario_idade   }
+dict_user = {   "primeiro_nome": nome_dict, 
+                "segundo_nome": sobrenome_dict, 
+                "idade": idade_dict   }
 
-dicionario_user_json = json.dumps(dicionario_user)
+dict_user_json = json.dumps(dict_user)
 
-print(dicionario_user_json)
+print(dict_user_json)

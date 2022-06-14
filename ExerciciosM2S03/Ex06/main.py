13d076d3fc8b2057679777f6791802fbae63cc10
Breno <hentgenp@gmail.com>
2022-06-14 03:16:11 -0300

    Update main.py
    Adicionado o código do exercicio, irei melhorar os códigos futuramente.

---
+++ ExerciciosM2S03/Ex06/main.py
@@ -0,0 +1,11 @@
+dicionario_1 = {'primeiro_nome': '', 'segundo_nome': '', 'idade': ''}
+print(dicionario_1)
+
+dicionario_2 = {'primeiro_nome': 'Uncle', 'segundo_nome': '', 'idade': ''}
+print(dicionario_2)
+
+dicionario_3 = {'primeiro_nome': 'Uncle', 'segundo_nome': ' Bob', 'idade': ''}
+print(dicionario_3)
+
+dicionario_4 = {'primeiro_nome': 'Uncle', 'segundo_nome': ' Bob', 'idade': '50'}
+print(dicionario_4)

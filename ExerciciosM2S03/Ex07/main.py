92160e3dac83468295be081432408710718bdf77
Breno <hentgenp@gmail.com>
2022-06-14 03:26:31 -0300

    Update main.py
    Mudado o nome na linha 3 de:
    
    dicionario_normal > dicionario

--- ExerciciosM2S03/Ex07/main.py
+++ ExerciciosM2S03/Ex07/main.py
@@ -1,7 +1,7 @@
 import json
 
-dicionario_normal = {'primeiro_nome': 'Uncle', 'segundo_nome': ' Bob', 'idade': '50'}
+dicionario = {'primeiro_nome': 'Uncle', 'segundo_nome': ' Bob', 'idade': '50'}
 
-dicionario_json = json.dumps(dicionario_normal)
+dicionario_json = json.dumps(dicionario)
 
 print(dicionario_json)

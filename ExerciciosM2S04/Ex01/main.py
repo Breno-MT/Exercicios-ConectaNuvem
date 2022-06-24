e98021affa07647ae5753113b88b3a7d8378f7cd
Breno <hentgenp@gmail.com>
2022-06-24 00:34:58 -0300

    Update main.py
    Colocado função de Saque, sendo possível sacar seu dinheiro sem ficar negativado.

--- ExerciciosM2S04/Ex01/main.py
+++ ExerciciosM2S04/Ex01/main.py
@@ -9,11 +9,34 @@ class Conta:
         self.__saldo += valor
 
 
+    def saque(self, valor):
+
+        # Verificações de negação
+        if valor > self.__saldo:
+            print("Saque Negado!!! Valor maior que o saldo!")
+        
+        elif valor > self.__saldo and valor > self.__saldo_limite:
+            print("Saque Negado!!! Valor maior que o saldo e o saldo limite!")
+        
+        elif valor > self.__saldo_limite:
+            print("Saque Negado!!! Valor maior que o limite de saldo!")
+        
+        # Verificações de validação
+        elif valor <= self.__saldo and valor <= self.__saldo_limite:
+            print(f"Saque Efetuado!!! No valor de R${valor:.2f}!")
+            self.__saldo -= valor
+        
+        elif valor <= self.__saldo:
+            print(f"Saque Efetuado!!! No valor de R${valor:.2f}!")
+            self.__saldo -= valor
+        
+
     # Este método especial irá printar a conta
     def __str__(self):
         return str(self.__dict__)
 
 
 conta_1 = Conta('Breno', '001')
-conta_1.depositar(1500)
+conta_1.depositar(8001)
+conta_1.saque(8000)
 print(conta_1)

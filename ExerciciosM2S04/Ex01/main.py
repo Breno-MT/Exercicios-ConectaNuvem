41888f40dbd3b2a46b4987c3fa0fa2c0a9c82b7e
Breno <hentgenp@gmail.com>
2022-06-24 00:20:00 -0300

    Update ex01

---
+++ ExerciciosM2S04/Ex01/main.py
@@ -0,0 +1,8 @@
+class Conta:
+    def __init__(self, nome_titular, numero, saldo, saldo_limite):
+        self.nome_titular = nome_titular
+        self.numero = numero
+        self.saldo = saldo
+        self.saldo_limite = saldo_limite
+
+        

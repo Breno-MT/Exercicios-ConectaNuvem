181610fa8a699408a7e5d015d75756e0e30b6c27
Breno <hentgenp@gmail.com>
2022-06-24 00:26:26 -0300

    Update main.py
    Adicionado função de depósito.

--- ExerciciosM2S04/Ex01/main.py
+++ ExerciciosM2S04/Ex01/main.py
@@ -2,13 +2,21 @@ class Conta:
     def __init__(self, nome_titular, numero):
         self.nome_titular = nome_titular
         self.numero = numero
-        self.__saldo = 150
+        self.__saldo = 0
         self.__saldo_limite = 8000
     
+
+    @property
+    def depositar(self, valor):
+        self.__saldo += valor
+
+
+
 
     # Este método especial irá printar a conta
     def __str__(self):
         return str(self.__dict__)
+
 
 conta_1 = Conta('Breno', '001')
 

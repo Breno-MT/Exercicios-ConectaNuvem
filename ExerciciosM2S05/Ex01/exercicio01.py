ce55dd55c54ce65064a237c2813db3fce823a09a
Breno <hentgenp@gmail.com>
2022-06-30 17:39:52 -0300

    Update

---
+++ ExerciciosM2S05/Ex01/exercicio01.py
@@ -0,0 +1,20 @@
+class Endereco:
+
+    def __init__(self, logradouro, numero, complemento, bairro, cidade, uf):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.bairro = bairro
+        self.cidade = cidade
+        self.uf = uf
+    
+    def cadastrar_endereco(self):
+        pass
+
+    def exibir_endereco(self):
+        pass
+
+    def salvar_endereco(self):
+        pass
+
+

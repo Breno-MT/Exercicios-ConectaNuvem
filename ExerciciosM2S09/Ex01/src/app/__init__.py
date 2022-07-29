930ca4b031ec8e578d27a5176b907c99df33d299
Breno <hentgenp@gmail.com>
2022-07-29 19:09:58 -0300

    Created Flask config

---
+++ ExerciciosM2S09/Ex01/src/app/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, jsonify
+
+from src.app.config import app_config
+
+
+app = Flask(__name__)
+
+app.config.from_object(app_config[os.getenv('FLASK_ENV')])
+
+
+@app.route("/")
+def hello_world(): 
+    return jsonify({"teste": 1})

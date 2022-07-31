54a31943105450d74d736e1f6b625379b1798d44
Breno <hentgenp@gmail.com>
2022-07-31 18:57:20 -0300

    Update Flask

--- ExerciciosM2S09/Ex01/src/app/controllers/users.py
+++ ExerciciosM2S09/Ex01/src/app/controllers/users.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 
-from src.app.db import db, User
+from src.app.db import db
+from src.app.utils import exists_key
 
 users = Blueprint('users', __name__, url_prefix="/users")
 
@@ -11,12 +12,32 @@ def list_users():
     return jsonify(db.all()), 200
 
 @users.route("/<int:id>", methods = ["GET"])
-def add_user(id):
+def find_user_id(id):
 
-    if id in db:
+    user = db.get(doc_id=id)
 
-        return jsonify(db.get(doc_id=id)), 200
-    
+    if user:
+        return {"Usuario procurado": user}, 200
+
     else:
         return jsonify({"error": f"O id {id} não existe."}), 404
+    
+@users.route("/create", methods = ['POST'])
+def create_user():
+
+    list_keys = ['nome', 'email', 'cpf']
+
+    data = exists_key(request.get_json(), list_keys)
+
+    if 'error' in data:
+        return jsonify(data), 400
+    
+    else:
+        insert = db.insert(data)
+
+        return {"id": insert, "user": data}, 202
+
+@users.route("/delete/<string:id>", methods = ["DELETE"])
+def delete_user():
+    pass
     
--- ExerciciosM2S09/Ex01/src/app/db/__init__.py
+++ ExerciciosM2S09/Ex01/src/app/db/__init__.py
@@ -1,7 +1,6 @@
+from requests import request
 from tinydb import TinyDB, Query
 
 User = Query()
 
 db = TinyDB('src/app/db/db.json')
-
-
